a3e665eb372d610bf3cdc10d07485b8cf1c5e974
Florian Privé <florian.prive.21@gmail.com>
2021-03-19 14:28:52 +0100

    print system() calls

--- knitr-options.R
+++ knitr-options.R
@@ -9,3 +9,11 @@ options(width = 75)
 
 Sys.setenv(LANGUAGE = "en")
 Sys.setlocale("LC_TIME", "C")
+
+system <- function(...) cat(base::system(..., intern = TRUE), sep = '\n')
+env_bigsnpr <- asNamespace("bigsnpr")
+rlang::env_unlock(env = env_bigsnpr)
+rlang::env_binding_unlock(env = env_bigsnpr)
+assign("system", system, envir = env_bigsnpr)
+rlang::env_binding_lock(env = env_bigsnpr)
+rlang::env_lock(env_bigsnpr)
